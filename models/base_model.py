083a49d476c1d7f51fb2c02de08f78617997f538
Biggestheart <arrhnnie@gmail.com>
2023-08-08 16:58:50 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,50 +1,63 @@
 #!/usr/bin/python3
-"""This module contains the base class for the AirBnB clone"""
+"""
+This module_holds a class BaseModel
+that defines all the common_attributes/methods for
+other_classes
+"""
+
+import models
+from datetime import datetime
 import uuid
-import datetime
-from models import storage
+
+date_format = "%Y-%m-%dT%H:%M:%S.%f"
 
 
-class BaseModel():
-    """Defines all the attributes and methods shared in the program"""
-
+class BaseModel:
+    """
+    Implementation_of BaseModel_class
+    """
     def __init__(self, *args, **kwargs):
-        """Initializing the class"""
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    # strptime creates a datetime obj from a str repr of date
-                    # creating datetime from the dict value string
-                    # created_at and updated_at
-                    self.__dict__["created_at"] = datetime.datetime.strptime(
-                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.datetime.strptime(
-                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
-        else:
-            self.id = str(uuid.uuid4())  # getting a random uuid for id
-            # setting the dates to the current date
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
-            storage.new(self)  # adding a call to the method new(self)
+        """
+        Initialization_of_class
+        """
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+            return
+
+        for key, value in kwargs.items():
+            if key != "__class__":
+                setattr(self, key, value)
+        if hasattr(self, "created_at") and type(self.created_at) is str:
+            self.created_at = datetime.strptime(kwargs["created_at"],
+                                                date_format)
+        if hasattr(self, "updated_at") and type(self.updated_at) is str:
+            self.updated_at = datetime.strptime(kwargs["updated_at"],
+                                                date_format)
 
     def __str__(self):
-        """returns a str representation of the object"""
-        class_name = self.__class__.__name__  # getting the class name
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        """
+        String_representation of the_class
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates updated_at with the current datetime"""
-        self.updated_at = datetime.datetime.now()
-        storage.save()  # calling save method on storage from models/__init__
+        """
+        updates_the public_instance attribute updated_at with the
+        current_datetime
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        """Returns a dict of key/value pairs of the __dict__ instance"""
-        the_dict = self.__dict__.copy()  # creating a copy of __dict__
-        # Storing values like classname, created_at and updated_at
-        the_dict["__class__"] = self.__class__.__name__
-        the_dict["created_at"] = self.created_at.isoformat()
-        the_dict["updated_at"] = self.updated_at.isoformat()
-        return the_dict
+        """
+        Returns a dictionary_containing all keys/values of __dict__
+        of the_instance
+        """
+        ndict = self.__dict__.copy()
+        ndict["__class__"] = self.__class__.__name__
+        ndict["created_at"] = datetime.isoformat(self.created_at)
+        ndict["updated_at"] = datetime.isoformat(self.updated_at)
+        return ndict
