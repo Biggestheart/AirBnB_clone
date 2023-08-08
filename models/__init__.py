a0c0fd1434b613240dd3ef5adf842c222f9296e3
Biggestheart <arrhnnie@gmail.com>
2023-08-08 16:56:06 -0700

    __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""The magic init file that turns a dir to a package"""
+"""
+Initialization models_package
+"""
+
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
